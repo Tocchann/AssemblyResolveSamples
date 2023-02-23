f650b923193aff810d6c22938cc81780308cb2c3
Toshiyuki TAKAHAGI <tosiyuki@hh.iij4u.or.jp>
2023-02-23 23:43:20 +0900

    プロセスアーキテクチャは実行中に変わることはないのでプロパティにしてパス解析はキャッシュする

--- AssemblyResolveLoader/Utility/AssemblyResolveLoader.cs
+++ AssemblyResolveLoader/Utility/AssemblyResolveLoader.cs
@@ -8,23 +8,39 @@ namespace DotNetLab.Utility
 {
 	/// <summary>
 	/// AssemblyResolveLoader
-	/// AppDomain.AssemblyResolve にハンドラを登録して自動的に環境依存モジュールをロードする。
+	/// プラットフォームアーキテクチャ依存アセンブリを動的にロードするための AssemblyResolve ハンドラ管理クラス
 	/// ロード対象は、.NET アセンブリである必要はあるが、C++/CLI プロジェクトである必要はない。
-	/// 利用時の注意事項
-	/// AssemblyName.Name の名前のDLLをロード対象としている(この実装ではリネームしない)
-	/// 切り分けは、System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture を参照する
-	/// X86 の場合は x86、X64 の場合は x64, Arm64の場合は ARM64
+	/// イベントハンドラ型なので、IDisposable を持つ。
+	/// アプリケーションのメイン関数の最初でusing して利用する
 	/// </summary>
 	public class AssemblyResolveLoader : IDisposable
 	{
+		/// <summary>
+		/// プロセスアーキテクチャに依存したモジュールを格納しているディレクトリ
+		/// </summary>
+		private string ArchitectureDependDirectory { get; }
 		public AssemblyResolveLoader()
 		{
 			Trace.WriteLine( $"AssemblyResolveLoader()" );
 			Console.WriteLine( $"AssemblyResolveLoader()" );
 
-			Trace.WriteLine( $"AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;" );
-			Console.WriteLine( $"AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;" );
-			AppDomain.CurrentDomain.AssemblyResolve += PreCall_AssemblyResolve;
+			Trace.WriteLine( $"RuntimeInformation.OSArchitecture={RuntimeInformation.OSArchitecture}" );
+			Console.WriteLine( $"RuntimeInformation.OSArchitecture={RuntimeInformation.OSArchitecture}" );
+			Trace.WriteLine( $"RuntimeInformation.ProcessArchitecture={RuntimeInformation.ProcessArchitecture}" );
+			Console.WriteLine( $"RuntimeInformation.ProcessArchitecture={RuntimeInformation.ProcessArchitecture}" );
+
+			// 実行中にプロセスアーキテクチャが変わることはない
+			ArchitectureDependDirectory = RuntimeInformation.ProcessArchitecture switch
+			{
+				Architecture.X86 => "x86",
+				Architecture.X64 => "x64",
+				Architecture.Arm64 => "ARM64",
+				Architecture.Arm => "ARM",
+				_ => throw new PlatformNotSupportedException( $"Unknown ProcessArchitecture({RuntimeInformation.ProcessArchitecture})" ),
+			};
+			Trace.WriteLine( $"ArchitectureDependDirectory={ArchitectureDependDirectory}" );
+			Console.WriteLine( $"ArchitectureDependDirectory={ArchitectureDependDirectory}" );
+			AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
 		}
 
 		private bool disposedValue;
@@ -36,44 +52,23 @@ namespace DotNetLab.Utility
 			{
 				if( disposing )
 				{
-					Trace.WriteLine( $"AppDomain.CurrentDomain.AssemblyResolve -= AssemblyResolve;" );
-					Console.WriteLine( $"AppDomain.CurrentDomain.AssemblyResolve -= AssemblyResolve;" );
-					AppDomain.CurrentDomain.AssemblyResolve -= PreCall_AssemblyResolve;
+					AppDomain.CurrentDomain.AssemblyResolve -= AssemblyResolve;
 				}
 				disposedValue = true;
 			}
 		}
 		public void Dispose()
 		{
-			Dispose( disposing: true );
+			Dispose( true );
 			GC.SuppressFinalize( this );
-		}
-		// ステップ実行した時にわかりやすくロードのタイミングを見せるために用意したダミーハンドラ
-		private Assembly PreCall_AssemblyResolve( object sender, ResolveEventArgs args )
-		{
-			//'ConsoleAppCore.exe'( CoreCLR: clrhost ): 'System.Runtime.InteropServices.RuntimeInformation.dll' が読み込まれました。
-			return AssemblyResolve( sender, args );
 		}
 		private Assembly AssemblyResolve( object sender, ResolveEventArgs args )
 		{
-			Trace.WriteLine( $"AssemblyResolve( args.Name={args.Name} )" );
-			Console.WriteLine( $"AssemblyResolve( args.Name={args.Name} )" );
+			var appDomain = sender as AppDomain;
+			Trace.WriteLine( $"AssemblyResolve( sender={appDomain?.FriendlyName}, args={args.Name} )" );
+			Console.WriteLine( $"AssemblyResolve( sender={appDomain?.FriendlyName}, args={args.Name} )" );
 			var assemblyName = new AssemblyName( args.Name );
-			Trace.WriteLine( $"RuntimeInformation.OSArchitecture={RuntimeInformation.OSArchitecture}" );
-			Console.WriteLine( $"RuntimeInformation.OSArchitecture={RuntimeInformation.OSArchitecture}" );
-			Trace.WriteLine( $"RuntimeInformation.ProcessArchitecture={RuntimeInformation.ProcessArchitecture}" );
-			Console.WriteLine( $"RuntimeInformation.ProcessArchitecture={RuntimeInformation.ProcessArchitecture}" );
-			// 位置は決め打ち。ファイル名はアセンブリ名と同じという前提にしている
-			var appendDir = RuntimeInformation.ProcessArchitecture switch
-			{
-				Architecture.X86 => "x86",
-				Architecture.X64 => "x64",
-				Architecture.Arm64 => "ARM64",
-				Architecture.Arm => "ARM",
-				_ => throw new PlatformNotSupportedException( $"Unknown ProcessArchitecture({RuntimeInformation.ProcessArchitecture})" ),
-			};
-			// 相対パス参照でよい
-			var targetPath = Path.Combine( appendDir, assemblyName.Name + ".dll" );
+			var targetPath = Path.Combine( ArchitectureDependDirectory, assemblyName.Name + ".dll" );
 			Trace.WriteLine( $"Assembly.LoadFrom( {targetPath} )" );
 			Console.WriteLine( $"Assembly.LoadFrom( {targetPath} )" );
 			var assembly = Assembly.LoadFrom( targetPath );
